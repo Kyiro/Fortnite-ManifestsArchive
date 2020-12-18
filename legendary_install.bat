7f786a2a41a24484d1f3e5915e4f98a782c864e5
realkyro <54126666+realkyro@users.noreply.github.com>
2020-12-18 07:15:39 +0100

    Update legendary_install.bat

--- legendary_install.bat
+++ legendary_install.bat
@@ -11,5 +11,5 @@ ECHO Where do you want your manifest to be downloaded?
 ECHO (Drag and Drop it into this window and then press enter)
 set /p install_path=""
 legendary -v -y uninstall --keep-files Fortnite
-legendary -v -y install --manifest "%~1" --download-only --no-install --game-folder "%install_path%" --base-url "https://epicgames-download1.akamaized.net/Builds/Fortnite/CloudDir" Fortnite
+legendary -v -y install --manifest "%~1" --download-only --no-install --game-folder %install_path% --base-url "https://epicgames-download1.akamaized.net/Builds/Fortnite/CloudDir" Fortnite
 pause
